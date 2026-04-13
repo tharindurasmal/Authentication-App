71e714aec6b90b5dfc26921032ff91d197a65f48
tharindu <tharindurasmal1@gmail.com>
2026-04-13 11:35:45 +0530

    login

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -11,7 +11,7 @@ class _Sign_InState extends State<Sign_In> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("login"),
+      child: Text("login pls"),
     );
   }
 }
